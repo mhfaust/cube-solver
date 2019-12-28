1f127f2bbbf65a7600ef12c3ad40a4a2c9987414
Michael Hildebrand-Faust <michael.hildebrand-faust@kroger.com>
2019-12-28 11:40:11 -0800

    solve fn

--- src/solve/layer0/index.js
+++
@@ -1,9 +0,0 @@
-// const { faceNames } = require('../../constants')
-
-// const faceHaving = (predicate, cube) => {
-//     return faceNames.find(faceName => predicate(cube[faceName]))
-// }
-
-// module.exports = {
-//     faceHaving
-// }
---
+++ src/solve/solve.js
@@ -0,0 +1,6 @@
+const { daisy } = require('./layer0/daisy/daisy')
+const { cross } = require('./layer0/cross/cross')
+
+function solve (cube) {
+
+}
