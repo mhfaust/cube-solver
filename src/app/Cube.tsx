80aed5e99116dfe53ca610ce4f345b3724d70160
Mike Hildebrand-Faust <mhfaust@gmail.com>
2023-10-12 01:36:04 -0700

    better colors

--- src/app/Cube.tsx
+++ src/app/Cube.tsx
@@ -13,11 +13,11 @@ import {
 import { RoundedBoxGeometry } from 'three/examples/jsm/geometries/RoundedBoxGeometry.js';
 // import { RoundedBoxGeometry } from 'threejs/examples/jsm/geometries/RoundedBoxGeometry.js';
 
-const red = new Color(1, 0, 0);
-const green = new Color(0, 1, 0);
-const blue = new Color(0, 0, 1);
+const red = new Color(.5, 0, 0);
+const green = new Color(0, .4, 0);
+const blue = new Color(.0, .02, 1);
 const orange = new Color(.8, .2, 0)
-const yellow = new Color(.5, .5, 0)
+const yellow = new Color(.7, .7, 0)
 const white = new Color(1, 1, 1)
 
 const colors = [red, orange, blue, green, white, yellow ]
@@ -32,7 +32,7 @@ type CubeProps = {
 }
 const Cube = ({ meshRef, x0, y0, z0 }: CubeProps) => {
   
-  const geometryRef = useRef(new RoundedBoxGeometry(1.02, 1.02, 1.02).toNonIndexed())
+  const geometryRef = useRef(new RoundedBoxGeometry(1.0, 1.0, 1.0).toNonIndexed())
   const geometry = geometryRef.current
   const materialRef = useRef(new MeshBasicMaterial({ vertexColors: true }))
   const material = materialRef.current
