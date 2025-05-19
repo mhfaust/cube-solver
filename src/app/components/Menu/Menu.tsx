b0f4fcc8a14d3ba925e4a693748833a65056904c
Mike Hildebrand-Faust <mike.hfaust@zengines.ai>
2025-05-18 22:28:35 -0700

    Fix TS compile error

--- src/app/components/Menu/Menu.tsx
+++ src/app/components/Menu/Menu.tsx
@@ -1,40 +1,39 @@
-import { themeNames } from '@/app/themes/useTheme'
-import styles from './Menu.module.css'
-import { useThemeName } from '@/app/store/selectors'
-import { useActions } from '@/app/store/useAppStore'
-import { useState } from 'react'
-import clsx from 'clsx'
+import { ThemeName, themeNames } from "@/app/themes/useTheme";
+import styles from "./Menu.module.css";
+import { useThemeName } from "@/app/store/selectors";
+import { useActions } from "@/app/store/useAppStore";
+import { useState } from "react";
+import clsx from "clsx";
 
 const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const [isOpen, setIsOpen] = useState(false)
-
-  const selectedThemeName = useThemeName()
-  const { setThemeName } = useActions()
+  const selectedThemeName = useThemeName();
+  const { setThemeName } = useActions();
 
   return (
     <div className={styles.container}>
       {isOpen && (
         <div className={styles.menu}>
-          {themeNames.map(name => (
-            <label 
+          {themeNames.map((name) => (
+            <label
               // for={name}
-              key={name} 
+              key={name}
             >
-              <input 
+              <input
                 // className={clsx({[styles.selected]: selectedThemeName === name})}
-                type="radio" 
+                type="radio"
                 // onChange={(isChecked) => setThemeName(name)}
-                onChange={(e) => setThemeName(e.target.value)}
+                onChange={(e) => setThemeName(e.target.value as ThemeName)}
                 checked={selectedThemeName === name}
                 value={name}
                 id={name}
                 className={styles.radio}
               />
-                {name}
+              {name}
             </label>
           ))}
-          <button 
+          <button
             onClick={() => setIsOpen(false)}
             className={styles.doneButton}
           >
@@ -44,16 +43,12 @@ const Menu = () => {
       )}
 
       {!isOpen && (
-        <div 
-          onClick={() => setIsOpen(true)}
-          className={styles.icon}
-        >
+        <div onClick={() => setIsOpen(true)} className={styles.icon}>
           Theme
         </div>
       )}
-
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
