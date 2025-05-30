382b00db3a3abdd65733c11410617bce3f78ce08
Mike Hildebrand-Faust <mike.hfaust@zengines.ai>
2025-05-30 08:07:06 -0700

    sramble back to 100

--- src/app/utils/useScramble.ts
+++ src/app/utils/useScramble.ts
@@ -17,7 +17,7 @@ const useScramble = () => {
     clearHistory()
 
     const randomSequence = Array.from(
-      { length: 1 }, 
+      { length: 100 }, 
       () => oneLayerSpins[Math.floor(Math.random() * numSpinTypes)]
     )
     const recurse = () => {
