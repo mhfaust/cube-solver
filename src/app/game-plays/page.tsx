97966078de88d82abe55f7e9a26fab7e541844c6
Mike Hildebrand-Faust <mike.hfaust@zengines.ai>
2025-06-04 12:13:01 -0700

    nav to game from list

--- src/app/game-plays/page.tsx
+++ src/app/game-plays/page.tsx
@@ -2,42 +2,57 @@
 'use client'
 
 import NoSsr from '@/components/NoSsr/NoSsr'
-import { useRecordsStore } from '@/store/recordsSlice'
+import { CubeHistory, useRecordsStore } from '@/store/recordsSlice'
 import { formatDate, reportTime } from '@/utils/displayTime';
 import styles from './game-plays.module.css'
-import { byAscending, byDescending } from '@/utils/sort';
+import { byAscending, byDescending, CompareFunction } from '@/utils/sort';
 import Link from 'next/link';
 import MainNav from '@/components/MainNav';
 import { countMutations } from '@/utils/history';
+import { useState } from 'react';
+import { clsx } from 'clsx';
+import { useNavigation } from '@/utils/useNavigation';
+
+type SortByField = 'duration' | 'moves' | 'date';
+
+// const sortStrategy: Record<SortByField, (game: CubeHistory, ) => 
 
 export default function App() {
 
+    const [sortby, setSortby] = useState<SortByField>('duration');
+    const sortStrategy: Record<SortByField, CompareFunction<CubeHistory>> = {
+        duration: byAscending(game => game.duration),
+        moves: byAscending(game => countMutations(game.moves)),
+        date: byDescending(game => game.startTime),
+    }
+
     const { records } = useRecordsStore();
-    const recordsByDuration = records.slice(0).sort(byAscending(record => record.duration))
-    const recordsByStartTime = records.slice(0).sort(byDescending(record => record.startTime))
-    const latestGame = recordsByStartTime[0];
+    const recordsSorted = records.slice(0).sort(sortStrategy[sortby])
+
+    const { goto } = useNavigation();
 
     return (
         <NoSsr>
             <div className={styles.main}>
-                <div style={{
-                    width:'500px', 
-                    // height:'100%',
-                    margin: 'auto', 
-                    display:"flex",
-                    flexDirection:"column",
-                    alignItems:"stretch"
-                }}>
-                    {recordsByDuration.map(({ startTime, moves, duration }) => (
-                        <div key={startTime} style={{
-                            display:"flex",
-                            flexDirection:"row",
-                            justifyContent:"space-between"
-                        }}>
-                            <span>{formatDate(startTime)}, &mdash; {countMutations(moves)} moves</span>
-                            <span>{reportTime(duration)}</span>
-                        </div>
-                    ))}
+                <div className={styles.list}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th onClick={() => setSortby('duration')}>Duration</th>
+                                <th onClick={() => setSortby('moves')}>Moves</th>
+                                <th onClick={() => setSortby('date')}>Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {recordsSorted.map(({ startTime, moves, duration, id }) => (
+                            <tr key={startTime} className={styles.item} onClick={() => goto.gamePlay(id)}>
+                                <td>{reportTime(duration)}</td>
+                                <td>{countMutations(moves)}</td>
+                                <td>{formatDate(startTime)}</td>
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <MainNav />
